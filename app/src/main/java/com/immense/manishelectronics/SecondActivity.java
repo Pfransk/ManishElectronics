4819dd43d09a1184ad7e7b141cab1b166394a976
pranav <pranavvsbeast@gmail.com>
2018-06-08 23:24:44 +0530

    Spinnner View for Second Activity. Region and Location

--- app/src/main/java/com/immense/manishelectronics/SecondActivity.java
+++ app/src/main/java/com/immense/manishelectronics/SecondActivity.java
@@ -3,32 +3,100 @@ package com.immense.manishelectronics;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
+
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import static android.R.layout.simple_spinner_item;
 
-public class SecondActivity extends AppCompatActivity{
-TextView tv;
-Button button;
+
+public class SecondActivity extends AppCompatActivity {
+    ArrayAdapter<CharSequence> arrayAdapter, arrayAdapter2;
+    TextView tv;
+    Button button;
+    MaterialBetterSpinner spinner1, spinner2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        Intent i =getIntent();
+
+        Intent i = getIntent();
         String s = i.getStringExtra("Name");
-tv = (TextView) findViewById(R.id.tv);
-button = (Button) findViewById(R.id.Signoutbutton);
-tv.setText(s);
-    button.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            FirebaseAuth.getInstance().signOut();
-            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-        }
-    });
+
+        tv = (TextView) findViewById(R.id.tv);
+        button = (Button) findViewById(R.id.Signoutbutton);
+        spinner1 = (MaterialBetterSpinner) findViewById(R.id.spinner1);
+        spinner2 = (MaterialBetterSpinner) findViewById(R.id.spinner2);
+        arrayAdapter = ArrayAdapter.createFromResource(this, R.array.Region, simple_spinner_item);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(arrayAdapter);
+        tv.setText(s);
+        ArrayAdapter<CharSequence> arrayAdapter2 = ArrayAdapter.createFromResource(this, R.array.SouthMumbai, simple_spinner_item);
+        arrayAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(arrayAdapter2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+
+        spinner1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+               String s1 = spinner1.getText().toString();
+               if(s1.equals("South Mumbai"))
+               {
+                   ArrayAdapter<CharSequence> arrayAdapter2 = ArrayAdapter.createFromResource(getBaseContext(), R.array.SouthMumbai, simple_spinner_item);
+                   arrayAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                   spinner2.setAdapter(arrayAdapter2);
+               }
+               else if(s1.equals("Eastern Suburbs"))
+               {
+                   ArrayAdapter<CharSequence> arrayAdapter2 = ArrayAdapter.createFromResource(getBaseContext(), R.array.EasternSuburbs, simple_spinner_item);
+                   arrayAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                   spinner2.setAdapter(arrayAdapter2);
+               }
+               else if(s1.equals("Western Suburbs"))
+               {
+                   ArrayAdapter<CharSequence> arrayAdapter2 = ArrayAdapter.createFromResource(getBaseContext(), R.array.WesternSuburbs, simple_spinner_item);
+                   arrayAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                   spinner2.setAdapter(arrayAdapter2);
+               }
+               else
+               {
+                   ArrayAdapter<CharSequence> arrayAdapter2 = ArrayAdapter.createFromResource(getBaseContext(), R.array.HarbourSuburbs, simple_spinner_item);
+                   arrayAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                   spinner2.setAdapter(arrayAdapter2);
+               }
+            }
+        });
     }
+
+
 
 }
